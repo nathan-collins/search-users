cd2baac8f67e9dc0290394d5a94fe73928e2b23d
Nathan Collins <nathan@unattended.tech>
2019-08-03 05:37:24 +1000

    Initial commit and code base setup

---
+++ src/search-users-app/search-users-app.js
@@ -0,0 +1,28 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class SearchUsersApp extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :host {
+          display: block;
+        }
+      </style>
+      <h2>Hello [[prop1]]!</h2>
+    `;
+  }
+  static get properties() {
+    return {
+      prop1: {
+        type: String,
+        value: 'search-users-app'
+      }
+    };
+  }
+}
+
+window.customElements.define('search-users-app', SearchUsersApp);
